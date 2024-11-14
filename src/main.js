fdf08144d6e90883db2818640b8fc37036acd787
Gabriel Bueno <contatobuenin@gmail.com>
2024-11-14 11:38:52 -0300

    colocado js bootstrap main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import './assets/js/global.js'
+// Importar o CSS do Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Importar o JavaScript do Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
