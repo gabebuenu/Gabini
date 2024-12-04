f49963cf5cc7b66f04dcedc61295c6625c1c6af6
BrunooBuenoo <128551954+BrunooBuenoo@users.noreply.github.com>
2024-12-04 16:48:09 -0300

    Post-footer

---
+++ node_modules/@chakra-ui/vue-theme/dist/declarations/src/foundations/z-index.d.ts
@@ -0,0 +1,18 @@
+declare const zIndices: {
+    hide: number;
+    auto: string;
+    base: number;
+    docked: number;
+    dropdown: number;
+    sticky: number;
+    banner: number;
+    overlay: number;
+    modal: number;
+    popover: number;
+    skipLink: number;
+    toast: number;
+    tooltip: number;
+};
+export declare type ZIndices = typeof zIndices;
+export default zIndices;
+//# sourceMappingURL=z-index.d.ts.map
